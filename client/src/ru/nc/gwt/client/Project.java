5ce9435251eb11091a27578cf18f64190d227316
SaydumarovVO <saidumarov@phystech.edu>
2015-12-17 01:19:35 +0300

    The version of project, that will work soon:)

--- client/src/ru/nc/gwt/client/Project.java
+++ client/src/ru/nc/gwt/client/Project.java
@@ -3,9 +3,14 @@ package ru.nc.gwt.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
+import ru.nc.gwt.server.VKMusicServer;
 
 /**
  * Created by Валера on 05.12.2015.
@@ -13,26 +18,7 @@ import com.google.gwt.user.client.ui.*;
 public class Project implements EntryPoint {
 
     public void onModuleLoad() {
-        Button button = new Button();
-        RootPanel.get().add(button);
-
-        final String kek = "паровозик";
-
-        button.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                GenServer.App.getInstance().generate(kek, new AsyncCallback<String>(){
-
-                    public void onFailure(Throwable caught) {
-
-                    }
-
-                    public void onSuccess(String result) {
-                        Window.alert(result);
-                    }
-                });
-            }
-        });
-
+        RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, "/server-1.0-SNAPSHOT/rest/music/listofaudio?action=makelist");
 
     }
 }
